fce8e0de2d10af6e777a7f79ecd42370542ac5cd
Alejandro Carreño Rubio <alejandro.cr473@gmail.com>
2020-10-06 19:36:34 +0200

    HelloWorld

---
+++ HelloWorldAlejandro/src/com/daw/alejandro/HelloWolrdAlejandro.java
@@ -0,0 +1,9 @@
+package com.daw.alejandro;
+
+public class HelloWolrdAlejandro {
+
+	public static void main(String[] args) {
+		System.out.println("Alejandro");
+		System.out.println("Hello World");
+	}
+}
